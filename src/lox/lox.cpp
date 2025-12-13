207c912ed6430775572da7873429a448bcaed89e
ellisra <robandrewellis@gmail.com>
2025-12-13 12:56:36 +0000

    Implemented string -> token map in scanner

--- src/lox/lox.cpp
+++ src/lox/lox.cpp
@@ -1,7 +1,12 @@
+#include <cstdlib>
+#include <cstring>
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <string_view>
+#include <vector>
+#include "error.hpp"
+#include "scanner.hpp"
 
 std::string read_file(std::string_view path) {
     std::ifstream file{path.data(), std::ios::in | std::ios::binary | std::ios::ate};
@@ -24,10 +29,39 @@ std::string read_file(std::string_view path) {
 }
 
 void run(std::string_view source) {
-    
+    Scanner scanner{source};
+    std::vector<Token> tokens = scanner.scan_tokens();
+
+    for (const Token &token : tokens) {
+        std::cout << token.to_string() << "\n";
+    }
 }
 
 void run_file(std::string_view path) {
     std::string contents = read_file(path);
-    run
+    run(contents);
+
+    if (had_error) std::exit(65);
+}
+
+void run_prompt() {
+    for (;;) {
+        std::cout << "> ";
+        std::string line;
+
+        if (!std::getline(std::cin, line)) break;
+        run(line);
+        had_error = false;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 2) {
+        std::cout << "Usage: lox [script]\n";
+        std::exit(64);
+    } else if (argc == 2) {
+        run_file(argv[1]);
+    } else {
+        run_prompt();
+    }
 }
--- src/lox/scanner.hpp
+++ src/lox/scanner.hpp
@@ -175,3 +175,22 @@ private:
         tokens.emplace_back(type, std::move(text), std::move(literal), line);
     }
 };
+
+inline const std::map<std::string, TokenType> Scanner::keywords = {
+    {"and",    TokenType::AND},
+    {"class",  TokenType::CLASS},
+    {"else",   TokenType::ELSE},
+    {"false",  TokenType::FALSE},
+    {"for",    TokenType::FOR},
+    {"fun",    TokenType::FUN},
+    {"if",     TokenType::IF},
+    {"nil",    TokenType::NIL},
+    {"or",     TokenType::OR},
+    {"print",  TokenType::PRINT},
+    {"return", TokenType::RETURN},
+    {"super",  TokenType::SUPER},
+    {"this",   TokenType::THIS},
+    {"true",   TokenType::TRUE},
+    {"var",    TokenType::VAR},
+    {"while",  TokenType::WHILE},
+};
